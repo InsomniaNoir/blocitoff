b0f0839464f0f96e28381227be04879abce78149
Jeremiah Shafer <jrshafer@mac.com>
2015-08-15 13:45:48 -0500

    Start basic front-end & authentication logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+  get "welcome/index"
+  get "welcome/about"
+
+  root to: 'welcome#index'
 end
